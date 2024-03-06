1bd174d0877626812c593960bf72cff8d0de01fe
Artheinian <156716293+Artheinian@users.noreply.github.com>
2024-03-06 16:04:46 -0700

    Test Saving

--- obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f589ac3a2c0007629fe3f770ddb9275b3211814f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c130ed78379f7868bd8c942d2b16773fbc84d38b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
